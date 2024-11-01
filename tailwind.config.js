bdb39d387e409cd8c5d5ef060be001b681e45313
Umut Yüksel <umtyuksell@icloud.com>
2024-11-01 17:39:52 +0300

    Finished Index View

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1024px',
+      '2xl' : '1024px',
+    },
+    extend: {
+      fontFamily: 
+      {
+        gemunu: ["Gemunu Libre", 'serif'], 
+        open: ["Open Sans", "serif"]
+      },
+      colors: {
+        "gega-red" : "#BC1A45",
+        "gega-melon" : "#FFD369",
+        "gega-grey" : "#DDDDDD",
+        "gega-white" : "#F7F7F7",
+      },
+      spacing: {
+        "128" : "32rem",
+      }
+    },
   },
   plugins: [],
 }
